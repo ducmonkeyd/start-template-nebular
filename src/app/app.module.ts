74458c8974b8464a5621ec863cf90da1d2b5c534
ngannguyen2001 <nguyenthikimngan2001tl@gmail.com>
2023-07-15 14:17:12 +0700

    commit header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
 import { SharedModule } from './shared/shared.module';
 import { ThemeModule } from './shared/theme.module';
-import { HeaderComponent } from './pages/layout/header/header.component';
-import { FooterComponent } from './pages/layout/footer/footer.component';
-import { SidebarComponent } from './pages/layout/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    SidebarComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
